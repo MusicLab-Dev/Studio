6ce37f8831dc149f18682d37eafacd19a9d6401c
Matthieu Moinvaziri <m.moinvaziri@gmail.com>
2021-09-16 23:28:59 +0200

    Added help

--- Studio/Studio.cpp
+++ Studio/Studio.cpp
@@ -134,6 +134,7 @@ void Studio::InitResources(void)
     Q_INIT_RESOURCE(Export);
     Q_INIT_RESOURCE(KeyboardShortcuts);
     Q_INIT_RESOURCE(Translations);
+    Q_INIT_RESOURCE(Help);
 }
 
 void Studio::DestroyResources(void)
@@ -153,6 +154,7 @@ void Studio::DestroyResources(void)
     Q_CLEANUP_RESOURCE(Export);
     Q_CLEANUP_RESOURCE(KeyboardShortcuts);
     Q_CLEANUP_RESOURCE(Translations);
+    Q_CLEANUP_RESOURCE(Help);
 
     Audio::PluginTable::Destroy();
     Audio::Device::ReleaseDriver();
